229cf73afe9e6015527480402da4f78e5cfdc434
EWINCP <98497723+EWINCP@users.noreply.github.com>
2025-01-04 11:30:01 -0600

    Add files via upload

---
+++ db.js
@@ -0,0 +1,16 @@
+require('dotenv').config();
+const { Client } = require('pg');
+
+const client = new Client({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+});
+
+client.connect()
+    .then(() => console.log('Conexión exitosa a PostgreSQL'))
+    .catch(err => console.error('Error de conexión a PostgreSQL', err.stack));
+
+module.exports = client;
