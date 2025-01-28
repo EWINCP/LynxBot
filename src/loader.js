a60a212d0a44c8e6d72194abd0f2a993622e326d
EWINCP <98497723+EWINCP@users.noreply.github.com>
2025-01-27 20:03:52 -0600

    Update loader.js

--- src/loader.js
+++ src/loader.js
@@ -1,23 +1,36 @@
-const { TIMEOUT_IN_MILLISECONDS_BY_EVENT } = require("./config");
-const { onMessagesUpsert } = require("./middlewares/onMesssagesUpsert");
+const { onMessagesUpsert } = require("./middlewares/onMessagesUpsert");
 const {
   onGroupParticipantsUpdate,
 } = require("./middlewares/onGroupParticipantsUpdate");
+const { TIMEOUT_IN_MILLISECONDS_BY_EVENT } = require("./config");
 
 exports.load = (socket) => {
+  // Manejo de eventos para mensajes
   socket.ev.on("messages.upsert", async ({ messages }) => {
-    setTimeout(() => {
-      onMessagesUpsert({ socket, messages });
-    }, TIMEOUT_IN_MILLISECONDS_BY_EVENT);
+    try {
+      // Simula un retraso para procesar los mensajes
+      await new Promise((resolve) =>
+        setTimeout(resolve, TIMEOUT_IN_MILLISECONDS_BY_EVENT),
+      );
+      await onMessagesUpsert({ socket, messages });
+    } catch (error) {
+      console.error("Error al procesar 'messages.upsert':", error);
+    }
   });
 
+  // Manejo de eventos para actualizaciones de participantes de grupo
   socket.ev.on("group-participants.update", async (data) => {
-    setTimeout(() => {
-      try {
-        onGroupParticipantsUpdate({ socket, groupParticipantsUpdate: data });
-      } catch (error) {
-        console.error(error);
-      }
-    }, TIMEOUT_IN_MILLISECONDS_BY_EVENT);
+    try {
+      // Simula un retraso para procesar la actualización de participantes
+      await new Promise((resolve) =>
+        setTimeout(resolve, TIMEOUT_IN_MILLISECONDS_BY_EVENT),
+      );
+      await onGroupParticipantsUpdate({
+        socket,
+        groupParticipantsUpdate: data,
+      });
+    } catch (error) {
+      console.error("Error al procesar 'group-participants.update':", error);
+    }
   });
 };
