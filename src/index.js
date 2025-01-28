d1064425f6502f60cc6924d13872cc8c7fbab9ac
EWINCP <98497723+EWINCP@users.noreply.github.com>
2025-01-27 20:03:04 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
 const { connect } = require("./connection");
 const { load } = require("./loader");
-const { infoLog, bannerLog } = require("./utils/logger");
+const { infoLog, errorLog, bannerLog } = require("./utils/logger");
 
 async function reconnectLoop() {
   let socket;
   while (true) {
     try {
+      // Verifica si ya hay una conexión activa antes de intentar conectar
+      if (socket && socket.isConnectedToWhatsApp) {
+        infoLog("Ya hay una conexión activa.");
+        return socket; // Si ya está conectado, no hace falta reconectar
+      }
+
       infoLog("Intentando conectar...");
       socket = await connect(); // Asegúrate de que connect() devuelva el socket
       infoLog("Conexión exitosa con WhatsApp.");
       break; // Sal del bucle si la conexión es exitosa
     } catch (error) {
       errorLog("Error al intentar conectar:", error.message);
-      warningLog("Reintentando en 5 segundos...");
+      infoLog("Reintentando en 5 segundos...");
       await new Promise((resolve) => setTimeout(resolve, 5000)); // Espera 5 segundos antes de reintentar
     }
   }
